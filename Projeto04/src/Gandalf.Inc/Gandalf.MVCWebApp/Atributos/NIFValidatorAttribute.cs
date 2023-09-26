30609de17d887d9801df597b446615a8384f46f0
Jônatas Afonso <jonatas.afonso@gmail.com>
2023-09-26 21:42:07 +0100

    Final da aula

---
+++ Projeto04/src/Gandalf.Inc/Gandalf.MVCWebApp/Atributos/NIFValidatorAttribute.cs
@@ -0,0 +1,31 @@
+﻿using Gandalf.MVCWebApp.Models;
+using Humanizer.Localisation;
+using System.ComponentModel.DataAnnotations;
+
+namespace Gandalf.MVCWebApp.Atributos
+{
+    public class NIFValidatorAttribute : ValidationAttribute
+    {
+        public NIFValidatorAttribute()
+        {
+                
+        }
+        public NIFValidatorAttribute(string nif) => NIF = nif;
+
+        public string NIF { get; }
+
+        public string GetErrorMessage() => $"Nif Inválido.";
+
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            var cliente = (Customer)validationContext.ObjectInstance;
+            
+            if (cliente.NumeroIdentificacaoFiscal.Length != 9) //Aqui entra a matemática...
+            {
+                return new ValidationResult(GetErrorMessage());
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
--- Projeto04/src/Gandalf.Inc/Gandalf.MVCWebApp/Models/Customer.cs
+++ Projeto04/src/Gandalf.Inc/Gandalf.MVCWebApp/Models/Customer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Gandalf.MVCWebApp.Atributos;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
@@ -8,17 +9,25 @@ public partial class Customer
 {
     public int CustomerId { get; set; }
 
+
+    [Required]
+    [MaxLength(150)]
     public string Nome { get; set; } = null!;
 
     public string? Endereco { get; set; }
 
     public string? Cidade { get; set; }
 
+
+    [MaxLength(255)]
+    [EmailAddress]
     public string? Email { get; set; }
 
-    [Required(ErrorMessage = "Ops, o NIF é obrigatório")]
-    [RegularExpression(@"\d{9}", ErrorMessage = "NIF inválido")]
+    //[Required(ErrorMessage = "Ops, o NIF é obrigatório")]
+    //[RegularExpression(@"\d{9}", ErrorMessage = "NIF inválido")]
     [Display(Name = "Número de Identificação Fiscal")]
+
+    [NIFValidator]
     public string NumeroIdentificacaoFiscal { get; set; } = null!;
 
 
